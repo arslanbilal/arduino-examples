d134277c89c01aad5defdaf62967ea78e5381628
arslanbilal <bilal.arslan1993@hotmail.com>
2015-11-30 22:54:35 +0200

    added  buzzer

---
+++ examples/02-buzzer/README.md
@@ -0,0 +1,18 @@
+# buzzer
+
+buzzer squeaks.
+
+![buzzer]
+
+This image made with [Fritzing].
+
+####pieces that used:
+1. arduino (uno/nano/mini/mega..)
+2. jumper wires
+3. buzzer
+4. 1 x 220 Ω resistor
+5. breadboard
+
+
+[buzzer]: https://github.com/arslanbilal/arduino-examples/raw/master/examples/02-buzzer/assets/buzzer.png "buzzer image"
+[Fritzing]: http://fritzing.org/home/
---
+++ examples/02-buzzer/buzzer/buzzer.ino
@@ -0,0 +1,18 @@
+//
+//  arduino-examples
+//  buzzer.ino
+//
+//  Created by Bilal Arslan on 30/11/15.
+//  Copyright (c) 2015 Bilal Arslan. All rights reserved.
+//
+
+
+const int BUZZER = 12; // buzzer pin address.
+
+void setup () {
+  pinMode(BUZZER,  OUTPUT); // setting buzzer pin, output mode.
+  digitalWrite(BUZZER, HIGH); // write high(1) for initial value.
+}
+
+void loop () {
+}
